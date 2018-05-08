34d4846d8771051fff908fe3f971c4ae0764e6f0
homi2015 <homi2015@users.noreply.github.com>
2018-05-08 15:51:38 +0530

    Added children
    added children to modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -4,7 +4,7 @@ import Box from "../direction/Box";
 import Icon from "../icon/Icon";
 import Button from "../button/Button";
 
-export default ({desktop}) => {
+export default ({desktop, children}) => {
   let classNames = [
     "modal",
     desktop ? "desktop" : "mobile"
@@ -21,11 +21,7 @@ export default ({desktop}) => {
           </Direction>
           <Box stretch classes="modal-content">
             <div>
-              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam urna, vehicula eu lorem sed, lobortis iaculis nulla. Nullam nulla diam, cursus eu est et, rutrum dictum tortor.</div>
-              <div>Phasellus sed interdum nisi, et hendrerit tellus. Quisque mattis lobortis tempor. Proin a nulla nec orci malesuada lacinia.</div>
-              <Button stretch size="l" decoration="action">{["Quit exercise series"]}</Button>
-              <Button stretch size="l" decoration="warning">{["Restart exercise series"]}</Button>
-              <Button stretch size="l" decoration="view">{["Don\'t quit"]}</Button>
+              {children}
             </div>
           </Box>
         </Direction>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import ContextNotificationShowcase from "./pages/ContextNotificationShowcase";
 import Direction from "./components/direction/Direction";
 import Box from "./components/direction/Direction";
 import Modal from "./components/modal/Modal";
+import Button from "./components/button/Button";
 
 import "./styles.scss";
 class App extends React.Component {
@@ -59,7 +60,13 @@ class App extends React.Component {
 						{this.comp(this.state.value)}
 					</Box>
 				</Direction>
-				<Modal desktop/>
+				<Modal desktop>
+					<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quam urna, vehicula eu lorem sed, lobortis iaculis nulla. Nullam nulla diam, cursus eu est et, rutrum dictum tortor.</div>
+					<div>Phasellus sed interdum nisi, et hendrerit tellus. Quisque mattis lobortis tempor. Proin a nulla nec orci malesuada lacinia.</div>
+					<Button stretch size="l" decoration="action">{["Quit exercise series"]}</Button>
+					<Button stretch size="l" decoration="warning">{["Restart exercise series"]}</Button>
+					<Button stretch size="l" decoration="view">{["Don\'t quit"]}</Button>
+				</Modal>
 			</div>
 		)
 	}
